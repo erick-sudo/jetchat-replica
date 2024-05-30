d52e91c38fe7388564d2eb92dc9da64aae8318fb
erick-sudo <erickochieng766@gmail.com>
2024-05-31 00:03:53 +0300

    Commit

--- app/src/main/java/com/compose/samples/replicas/jetchat/profile/Profile.kt
+++ app/src/main/java/com/compose/samples/replicas/jetchat/profile/Profile.kt
@@ -1,25 +1,40 @@
 package com.compose.samples.replicas.jetchat.profile
 
+import android.widget.Space
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.Chat
+import androidx.compose.material.icons.outlined.Chat
+import androidx.compose.material.icons.outlined.Create
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
@@ -28,9 +43,14 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.compose.samples.replicas.jetchat.FunctionalityNotAvailablePopup
+import com.compose.samples.replicas.jetchat.components.JetchatDivider
+import com.compose.samples.replicas.jetchat.components.baselineHeight
+import com.compose.samples.replicas.jetchat.R
+import com.compose.samples.replicas.jetchat.components.AnimatingFabContent
 
 @Composable
 fun ProfileScreen(
@@ -61,12 +81,25 @@ fun ProfileScreen(
                     data = userData,
                     containerHeight = this@BoxWithConstraints.maxHeight
                 )
+
+                UserInfoFields(
+                    userData = userData,
+                    containerHeight = this@BoxWithConstraints.maxHeight
+                )
             }
         }
 
         val fabExtended by remember { derivedStateOf { scrollState.value == 0 } }
 
-
+        ProfileFab(
+            extended = fabExtended,
+            userIsMe = userData.isMe(),
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                // Offsets the FAB to compensate for CoordinatorLayout collapsing behaviour
+                .offset(y = ((-100).dp)),
+            onFabClicked = { functionalityNotAvailablePopupShown = true }
+        )
     }
 }
 
@@ -94,5 +127,161 @@ private fun ProfileHeader(
             contentScale = ContentScale.Crop,
             contentDescription = null
         )
+    }
+}
+
+@Composable
+private fun UserInfoFields(
+    userData: ProfileScreenState,
+    containerHeight: Dp
+) {
+    Column {
+        Spacer(modifier = Modifier.height(8.dp))
+
+        NameAndPosition(userData)
+
+        ProfileProperty(
+            label = stringResource(id = R.string.display_name),
+            value = userData.displayName
+        )
+
+        ProfileProperty(
+            label = stringResource(id = R.string.status),
+            value = userData.status
+        )
+
+        ProfileProperty(
+            label = stringResource(id = R.string.twitter),
+            value = userData.twitter,
+            isLink = true
+        )
+
+        userData.timeZone?.let {
+            ProfileProperty(
+                label = stringResource(id = R.string.timezone),
+                value = it
+            )
+        }
+
+        // Add a spacer that always shows part (320.dp) of the fields list regardless of the device,
+        // in order to always leave some content at the top.
+        Spacer(Modifier.height((containerHeight - 320.dp).coerceAtLeast(0.dp)))
+    }
+}
+
+@Composable
+private fun NameAndPosition(
+    userData: ProfileScreenState
+) {
+    Column(modifier = Modifier.padding(horizontal = 16.dp)) {
+        Name(
+            userData,
+            modifier = Modifier.baselineHeight(32.dp)
+        )
+
+        Position(
+            userData = userData,
+            modifier = Modifier
+                .padding(bottom = 20.dp)
+                .baselineHeight(24.dp)
+        )
+    }
+}
+
+@Composable
+private fun Name(
+    userData: ProfileScreenState,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = userData.name,
+        modifier = modifier,
+        style = MaterialTheme.typography.headlineSmall
+    )
+}
+
+@Composable
+private fun Position(
+    userData: ProfileScreenState,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = userData.name,
+        modifier = modifier,
+        style = MaterialTheme.typography.bodyLarge,
+        color = MaterialTheme.colorScheme.onSurfaceVariant
+    )
+}
+
+@Composable
+fun ProfileProperty(
+    label: String,
+    value: String,
+    isLink: Boolean = false
+) {
+    Column(
+        modifier = Modifier
+            .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+    ) {
+        JetchatDivider()
+
+        Text(
+            text = label,
+            modifier = Modifier.baselineHeight(24.dp),
+            style = MaterialTheme.typography.bodySmall,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+
+        val style = if(isLink) {
+            MaterialTheme.typography.bodyLarge.copy(color = MaterialTheme.colorScheme.primary)
+        } else {
+            MaterialTheme.typography.bodyLarge
+        }
+
+        Text(
+            text = value,
+            modifier = Modifier
+                .baselineHeight(24.dp),
+            style = style
+        )
+    }
+}
+
+@Composable
+fun ProfileFab(
+    extended: Boolean,
+    userIsMe: Boolean,
+    modifier: Modifier = Modifier,
+    onFabClicked: () -> Unit = { }
+) {
+    key(userIsMe) {
+        FloatingActionButton(
+            onClick = onFabClicked,
+            modifier = modifier
+                .padding(16.dp)
+                .navigationBarsPadding()
+                .height(48.dp)
+                .widthIn(min = 48.dp),
+            containerColor = MaterialTheme.colorScheme.tertiaryContainer
+        ) {
+            AnimatingFabContent(
+                icon = {
+                    Icon(
+                        imageVector = if (userIsMe) Icons.Outlined.Create else Icons.AutoMirrored.Outlined.Chat,
+                        contentDescription = stringResource(
+                            if (userIsMe) R.string.edit_profile else R.string.message
+                        )
+                    )
+                },
+                text = {
+                    Text(
+                        text = stringResource(
+                            id = if (userIsMe) R.string.edit_profile else R.string.message
+                        ),
+                    )
+                },
+                extended = extended
+            )
+        }
     }
 }
